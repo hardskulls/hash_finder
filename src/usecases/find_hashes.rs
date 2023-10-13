c4da3b72e56276f3a15bf4cb2f40100e1258e952
hardskulls <hardskulls@rambler.ru>
2023-10-13 15:47:49 +0300

    fix(usecases/find_hashes): Fixed docs;

--- src/usecases/find_hashes.rs
+++ src/usecases/find_hashes.rs
@@ -2,7 +2,7 @@ use crate::core::hashing::abstractions::gen_range::{GenRange, SingleNumAlias};
 use crate::core::hashing::abstractions::hasher::{HashEndsWithNZeros, RingHasher};
 use crate::core::hashing::types::numbers::{Number, NumberHash};
 
-/// Finds hashes containing enough zeros at the end and sends them through channel.
+/// Finds hashes containing enough zeros at the end and applies `f` to the result.
 pub fn find_hashes<F>(start: Number, end: Number, with_zeros_at_end: usize, f: F)
 where
     F: Fn(NumberHash<Number, String>) + Send + Sync,
